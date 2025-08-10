7a29960af1e098c251c987c5733094d7ec5287a3
xibsx <xibstech@gmail.com>
2025-08-10 10:50:36 +0300

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,18 @@
+{
+  "name": "orion-bot",
+  "version": "1.0.0",
+  "description": "WhatsApp Multi-Device Bot with OTP Pairing",
+  "main": "src/bot.js",
+  "scripts": {
+    "start": "node src/bot.js",
+    "dev": "nodemon src/bot.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.5.0",
+    "chalk": "^5.3.0",
+    "dotenv": "^16.3.1"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.2"
+  }
+}
